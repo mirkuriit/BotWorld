5671d7d4904a9787e18b919ed5ae92b96cb94dc7
mirkuriit <e.daurtcev@dianapp.ru>
2025-06-17 05:55:40 +0300

    BotWorld add moving and swaping game object. Put cell_auto algo into cell_auto.py

--- cell_auto.py
+++ cell_auto.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import settings
+from pygame import Rect
 from random import randint, choice
 from enum import Enum
 from pprint import pprint
@@ -15,32 +16,30 @@ class PlayerType(Enum):
     NEFOR = (0, 0, 255)
     DEAD = (0, 0, 0)
 
-def init_sprites():
-    sprites = {}
-    scaled_player_sprite = pg.transform.scale(
-        pg.image.load("img.png"),
-        (settings.IMG_SCALE, settings.IMG_SCALE)
-    )
 
-    for player_type in PlayerType:
-        colored_sprite = scaled_player_sprite.copy()
-        colored_sprite.fill(
-            player_type.value, special_flags=pg.BLEND_MULT)
-        sprites[player_type] = colored_sprite
-
-    return sprites
-
-SPRITES = init_sprites()
-
-print(SPRITES)
-
-class Player:
-    def __init__(self, scaler, x: int, y: int, t: PlayerType):
+class ScaledRect(Rect):
+    def __init__(self, scaler, x, y):
+        super().__init__(x * scaler, y * scaler, scaler, scaler)
         self.scaler = scaler
         self.x = x
         self.y = y
+        self.color = (255, 0, 0)
+
+    def draw(self):
+        pg.draw.rect(
+            screen,
+            self.color,
+            Rect(
+                self.x * self.scaler, self.y * self.scaler,
+                self.scaler, self.scaler)
+        )
+
+
+class Player(ScaledRect):
+    def __init__(self, scaler, x: int, y: int, t: PlayerType):
+        super().__init__(scaler, x, y)
         self.type: PlayerType = t
-        self.sprite = SPRITES[self.type] if self.type != PlayerType.DEAD else None
+        self.color = self.type.value
 
     def is_dead(self):
         return self.type == PlayerType.DEAD
@@ -50,15 +49,9 @@ class Player:
 
     def dead(self):
         self.type = PlayerType.DEAD
-        self.sprite = SPRITES[self.type]
 
     def born(self):
         self.type = PlayerType.NEFOR
-        self.sprite = SPRITES[self.type]
-
-    def draw(self):
-        if self.is_alive():
-            screen.blit(self.sprite, (self.x * self.scaler, self.y * self.scaler))
 
     def live(self):
         match self.type:
@@ -88,7 +81,7 @@ class Player:
         return self.type.name
 
 
-class World:
+class World(list):
     def __init__(self, width, height, dead_coef, fill_strategy="random"):
         self.width = width
         self.height = height
@@ -99,7 +92,7 @@ class World:
             self.height,
             self.fill_strategy,
             self.dead_coef
-        )
+            )
 
     def generate_world(
             self,
@@ -107,35 +100,33 @@ class World:
             height: int,
             fill_strategy: str,
             dead_coef: int
-    ) -> list[list[Player]]:
+            ) -> list[list[Player]]:
         num_players = width * height
         player_types = [PlayerType.DEAD] * int(dead_coef * num_players) + [PlayerType.NEFOR] * int(
             (1 - dead_coef) * num_players)
         if len(player_types) > num_players:
             player_types = player_types[::-1][:num_players]
         elif len(player_types) < num_players:
-            player_types += [PlayerType.DEAD] * (
-                    num_players - len(player_types))
-        world = [
-            [
-            Player(scaler=settings.IMG_SCALE,
-                   x=i,
-                   y=j,
-                   t=player_types.pop(randint(0, len(player_types) - 1))) for i in
-            range(width)
-            ]
-            for j in range(height)]
+            player_types += [PlayerType.DEAD] * (num_players - len(player_types))
+        world = [[Player(
+            scaler=settings.IMG_SCALE,
+            x=i,
+            y=j,
+            t=player_types.pop(randint(0, len(player_types) - 1))) for i in range(width)] for j in range(height)]
         return world
 
     @classmethod
     def from_world(cls, source_world):
-        new_world = cls.__new__(cls)
+        """Фабричный метод для создания копии мира"""
+        new_world = cls.__new__(cls)  # Создаем экземпляр без вызова __init__
 
+        # Копируем все атрибуты
         new_world.width = source_world.width
         new_world.height = source_world.height
         new_world.dead_coef = source_world.dead_coef
         new_world.fill_strategy = source_world.fill_strategy
 
+        # Глубокое копирование игроков
         new_world.world = [
             [
                 Player(
@@ -154,8 +145,7 @@ class World:
     def draw(self):
         for row in self.world:
             for player in row:
-                if player.is_alive():
-                    player.draw()
+                player.draw()
 
     def __repr__(self):
         s = ""
@@ -185,32 +175,32 @@ def get_neighbours(player: Player, world: World) -> list[Player]:
 
 def step(OLD_WORLD, NEW_WORLD):
     for i in range(OLD_WORLD.height):
-        for j in range(OLD_WORLD.width):
+        for j in range(OLD_WORLD.height):
             player = NEW_WORLD.world[i][j]
             neighbours = get_neighbours(player, OLD_WORLD)
 
             ### TODO in other function
-            num_alive = sum(n.is_alive() for n in neighbours)
-            if (num_alive < 2 or num_alive > 3) and player.is_alive():
+            num_alive = sum(i.is_alive() for i in neighbours)
+            if num_alive < 2 or num_alive > 3 and player.is_alive():
                 player.dead()
             elif num_alive == 3 and player.is_dead():
                 player.born()
     return NEW_WORLD
 
 
-OLD_WORLD: World = World(
-    settings.WORLD_WIDTH, settings.WORLD_HEIGHT, dead_coef=settings.DEAD_COEF)
+OLD_WORLD: World = World(settings.WORLD_WIDTH, settings.WORLD_HEIGHT, dead_coef=settings.DEAD_COEF)
 
-pg.init()
 screen = pg.display.set_mode(settings.RES)
 clock = pg.time.Clock()
 
-
+# for i in range(10):
+#     print(get_neighbours((OLD_WORLD.world[2][i]), OLD_WORLD), OLD_WORLD.world[2][i].x, OLD_WORLD.world[2][i].y)
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
-
+    # OLD_WORLD.copy()
+    # NEW_WORLD = step(OLD_WORLD, NEW_WORLD)
     NEW_WORLD = World.from_world(OLD_WORLD)
     OLD_WORLD = step(OLD_WORLD, NEW_WORLD)
 
--- main.py
+++ main.py
@@ -1,43 +1,68 @@
 import pygame as pg
 import settings
-from pygame import Rect
 from random import randint, choice
 from enum import Enum
 from pprint import pprint
 import copy
-import numpy as np
+import time
 
 from abc import abstractmethod
 
+
 class PlayerType(Enum):
     AGRESSIVE = (255, 0, 0)
+    APPLE = (170, 50, 20)
     FRIENDLY = (0, 255, 0)
     NEFOR = (0, 0, 255)
     DEAD = (0, 0, 0)
 
 
-class ScaledRect(Rect):
-    def __init__(self, scaler, x, y):
-        super().__init__(x*scaler, y*scaler, scaler, scaler)
+def init_sprites():
+    sprites = {}
+
+    scaled_nefor_sprite = pg.transform.scale(
+        pg.image.load("player.png"),
+        (settings.IMG_SCALE, settings.IMG_SCALE)
+    )
+    scaled_nefor_sprite.fill(
+        PlayerType.NEFOR.value, special_flags=pg.BLEND_MULT
+    )
+
+    scaled_friendly_sprite = pg.transform.scale(
+        pg.image.load("player.png"),
+        (settings.IMG_SCALE, settings.IMG_SCALE)
+    )
+    scaled_friendly_sprite.fill(
+        PlayerType.FRIENDLY.value, special_flags=pg.BLEND_MULT
+    )
+
+    apple = pg.transform.scale(
+        pg.image.load("food.png"),
+        (settings.IMG_SCALE, settings.IMG_SCALE)
+        )
+    apple.fill(
+        PlayerType.APPLE.value, special_flags=pg.BLEND_MULT)
+
+    sprites[PlayerType.APPLE] = apple
+    sprites[PlayerType.NEFOR] = scaled_nefor_sprite
+    sprites[PlayerType.FRIENDLY] = scaled_friendly_sprite
+
+    return sprites
+
+
+SPRITES = init_sprites()
+
+print(SPRITES)
+
+
+class GameObject:
+    def __init__(self, scaler, x: int, y: int, t: PlayerType):
         self.scaler = scaler
         self.x = x
         self.y = y
-        self.color = (255, 0, 0)
-
-    def draw(self):
-        pg.draw.rect(
-            screen,
-            self.color,
-            Rect(self.x*self.scaler, self.y*self.scaler,
-                 self.scaler, self.scaler)
-        )
-
-
-class Player(ScaledRect):
-    def __init__(self, scaler, x: int, y: int, t: PlayerType):
-        super().__init__(scaler, x, y)
         self.type: PlayerType = t
-        self.color = self.type.value
+        self.sprite = SPRITES[self.type] if self.type != PlayerType.DEAD else None
+        self.has_move = True
 
     def is_dead(self):
         return self.type == PlayerType.DEAD
@@ -45,12 +70,50 @@ class Player(ScaledRect):
     def is_alive(self):
         return not (self.type == PlayerType.DEAD)
 
+    def draw(self):
+        if self.is_alive():
+            screen.blit(self.sprite, (self.x * self.scaler, self.y * self.scaler))
+
     def dead(self):
         self.type = PlayerType.DEAD
+        self.sprite = SPRITES[self.type]
 
-    def born(self):
-        self.type = PlayerType.NEFOR
+    @abstractmethod
+    def live(self):
+        pass
 
+
+class Food(GameObject):
+    def __init__(self, scaler, x: int, y: int, t: PlayerType):
+        super().__init__(scaler, x, y, t)
+
+    def live(self):
+        print("apple move")
+
+
+class Player(GameObject):
+    def __init__(self, scaler, x: int, y: int, t: PlayerType):
+        super().__init__(scaler, x, y, t)
+        self.hp = 100
+        self.speed = 1
+
+    def increase_level(self):
+        self.hp += 1
+
+    def decrease_level(self):
+        self.hp -= 1
+
+    def move_right(self):
+        self.x += self.speed
+
+    def move_left(self):
+        self.x -= self.speed
+
+    def move_up(self):
+        self.y -= self.speed
+
+    def move_down(self):
+        self.y += self.speed
 
     def live(self):
         match self.type:
@@ -60,9 +123,10 @@ class Player(ScaledRect):
                 self._friendly_move()
             case PlayerType.NEFOR:
                 self._nefor_move()
+        self.has_move = False
 
     def _nefor_move(self):
-        pass
+        print("nefor_move")
 
     def _argessive_move(self):
         pass
@@ -71,7 +135,40 @@ class Player(ScaledRect):
         pass
 
     def _friendly_move(self):
-        pass
+        while True:  # Ждём любое событие
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    return False
+
+                if event.type == pg.KEYDOWN:  # Только событие нажатия
+                    if event.key == pg.K_LEFT:
+                        print('Левая стрелка', f"{self.x}, {self.y}")
+                        OLD_WORLD.swap_game_object(self.x, self.y, self.x - 1, self.y)
+                        print('Левая стрелка', f"{self.x}, {self.y}")
+                        return True
+                    elif event.key == pg.K_RIGHT:
+                        print('Правая стрелка', f"{self.x}, {self.y}")
+
+                        OLD_WORLD.swap_game_object(self.x, self.y, self.x + 1, self.y)
+                        print('Правая стрелка', f"{self.x}, {self.y}")
+
+                        return True
+                    elif event.key == pg.K_UP:
+                        print('Стрелка вверх', f"{self.x}, {self.y}")
+                        OLD_WORLD.swap_game_object(self.x, self.y, self.x, self.y - 1)
+                        print('Стрелка вверх', f"{self.x}, {self.y}")
+                        return True
+                    elif event.key == pg.K_DOWN:
+                        print('Стрелка вниз', f"{self.x}, {self.y}")
+                        OLD_WORLD.swap_game_object(self.x, self.y, self.x, self.y + 1)
+                        print('Стрелка вниз', f"{self.x}, {self.y}")
+                        return True
+
+            # Не нагружаем процессор в ожидании
+            pg.time.delay(100)
+        # self.move_right()
+        print("friendly move")
 
     def __repr__(self):
         return f"x:{self.x} y:{self.y} {self.type.name}"
@@ -80,48 +177,55 @@ class Player(ScaledRect):
         return self.type.name
 
 
-
-
-class World(list):
+class World:
     def __init__(self, width, height, dead_coef, fill_strategy="random"):
         self.width = width
         self.height = height
         self.fill_strategy = fill_strategy
         self.dead_coef = dead_coef
-        self.world: list[list[Player]]  = self.generate_world(self.width,
-                                         self.height,
-                                         self.fill_strategy,
-                                         self.dead_coef
-                                                              )
-    def generate_world(self,
-                       width: int,
-                       height: int,
-                       fill_strategy: str,
-                       dead_coef: int) -> list[list[Player]]:
+        self.world: list[list[Player]] = self.generate_world(
+            self.width,
+            self.height,
+            self.fill_strategy,
+            self.dead_coef
+        )
+
+    def generate_world(
+            self,
+            width: int,
+            height: int,
+            fill_strategy: str,
+            dead_coef: int
+    ) -> list[list[Player]]:
         num_players = width * height
-        player_types = [PlayerType.DEAD] * int(dead_coef * num_players) + [PlayerType.NEFOR] * int((1 - dead_coef) * num_players)
+        player_types = [PlayerType.DEAD] * int(dead_coef * num_players) + [PlayerType.NEFOR] * 4 + [
+            PlayerType.APPLE] * 4 + [PlayerType.FRIENDLY]
         if len(player_types) > num_players:
             player_types = player_types[::-1][:num_players]
         elif len(player_types) < num_players:
-            player_types += [PlayerType.DEAD] * (num_players - len(player_types))
-        world = [[Player(scaler=settings.RECT_SCALE,
-                             x=i,
-                             y=j,
-                             t=player_types.pop(randint(0, len(player_types) - 1))) for i in range(width)] for j in range(height)]
+            player_types += [PlayerType.DEAD] * (
+                    num_players - len(player_types))
+        world = [
+            [
+                Player(
+                    scaler=settings.IMG_SCALE,
+                    x=i,
+                    y=j,
+                    t=player_types.pop(randint(0, len(player_types) - 1))) for i in
+                range(width)
+            ]
+            for j in range(height)]
         return world
 
     @classmethod
     def from_world(cls, source_world):
-        """Фабричный метод для создания копии мира"""
-        new_world = cls.__new__(cls)  # Создаем экземпляр без вызова __init__
+        new_world = cls.__new__(cls)
 
-        # Копируем все атрибуты
         new_world.width = source_world.width
         new_world.height = source_world.height
         new_world.dead_coef = source_world.dead_coef
         new_world.fill_strategy = source_world.fill_strategy
 
-        # Глубокое копирование игроков
         new_world.world = [
             [
                 Player(
@@ -140,7 +244,18 @@ class World(list):
     def draw(self):
         for row in self.world:
             for player in row:
-                player.draw()
+                if player.is_alive():
+                    player.draw()
+
+    def swap_game_object(self, x1, y1, x2, y2):
+
+        obj1 = self.world[y1][x1]
+        obj2 = self.world[y2][x2]
+
+        self.world[y1][x1], self.world[y2][x2] = obj2, obj1
+
+        obj1.x, obj2.x = x2, x1
+        obj1.y, obj2.y = y2, y1
 
     def __repr__(self):
         s = ""
@@ -148,64 +263,56 @@ class World(list):
             s += ("\t".join(map(str, row)) + "\n")
         return s
 
+
 def get_neighbours(player: Player, world: World) -> list[Player]:
     px, py = player.x, player.y
     neighbours_tmp = [
-        (px+1, py+1),
-        (px, py+1),
-        (px-1, py+1),
-        (px+1, py),
-        (px-1, py),
-        (px-1, py-1),
-        (px, py-1),
-        (px+1, py-1),
-        ]
+        (px + 1, py + 1),
+        (px, py + 1),
+        (px - 1, py + 1),
+        (px + 1, py),
+        (px - 1, py),
+        (px - 1, py - 1),
+        (px, py - 1),
+        (px + 1, py - 1),
+    ]
     neighbours_coords = [(x, y) for x, y in neighbours_tmp if
-                  (x >= 0) and (y >= 0) and \
-                  (x <= world.width-1) and (y <= world.height-1) ]
+                         (x >= 0) and (y >= 0) and \
+                         (x <= world.width - 1) and (y <= world.height - 1)]
     neighbors = [world.world[y][x] for x, y in neighbours_coords]
     return neighbors
 
-def step(OLD_WORLD, NEW_WORLD):
-    for i in range(OLD_WORLD.height):
-        for j in range(OLD_WORLD.height):
-            player = NEW_WORLD.world[i][j]
-            neighbours = get_neighbours(player, OLD_WORLD)
 
-            ### TODO in other function
-            num_alive = sum(i.is_alive() for i in neighbours)
-            if num_alive < 2 or num_alive > 3 and player.is_alive():
-                player.dead()
-            elif num_alive == 3 and player.is_dead():
-                player.born()
+def step(OLD_WORLD, NEW_WORLD):
     return NEW_WORLD
 
 
-OLD_WORLD: World = World(settings.WORLD_WIDTH, settings.WORLD_HEIGHT, dead_coef=settings.DEAD_COEF)
+OLD_WORLD: World = World(
+    settings.WORLD_WIDTH, settings.WORLD_HEIGHT, dead_coef=settings.DEAD_COEF)
 
+pg.init()
 screen = pg.display.set_mode(settings.RES)
 clock = pg.time.Clock()
 
-
-# for i in range(10):
-#     print(get_neighbours((OLD_WORLD.world[2][i]), OLD_WORLD), OLD_WORLD.world[2][i].x, OLD_WORLD.world[2][i].y)
+print(OLD_WORLD)
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
-    # OLD_WORLD.copy()
-    # NEW_WORLD = step(OLD_WORLD, NEW_WORLD)
-    NEW_WORLD = World.from_world(OLD_WORLD)
-    OLD_WORLD = step(OLD_WORLD, NEW_WORLD)
-
-
-
-
-
     clock.tick(settings.FPS)
     pg.display.set_caption(f'FPS: {clock.get_fps()}')
     screen.fill((0, 0, 0))
+
+    # NEW_WORLD = World.from_world(OLD_WORLD)
+    # OLD_WORLD = step(OLD_WORLD, OLD_WORLD)
+
     OLD_WORLD.draw()
     pg.display.flip()
-
-
+    for row in OLD_WORLD.world:
+        for player in row:
+            player.has_move = True
+    for row in OLD_WORLD.world:
+        for player in row:
+            if player.has_move:
+                player.live()
+    print(OLD_WORLD)
